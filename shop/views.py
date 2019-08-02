2d63f782585b0f75c36e67c2d84ac0cabd1b57d4
tuipik <tuipiku@gmail.com>
2019-08-02 17:52:44 +0300

    added base html template, css, js and view

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from shop.models import Category, Brand, Product
 
 
 def base_view(request):
-    return render(request, 'base.html', {})
+    categories = Category.objects.all()
+    brands = Brand.objects.all()
+    products = Product.objects.all()
+
+    context = {
+        'categories': categories,
+        'brands': brands,
+        'products': products
+    }
+
+    return render(request, 'base.html', context)
